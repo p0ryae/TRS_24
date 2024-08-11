81d9a4e736a5e62abe1a868be308887218710d94
p0ryae <theporyafm12@gmail.com>
2024-08-11 00:19:53 -0700

    Ready for new publish

--- src/lib.rs
+++ src/lib.rs
@@ -244,24 +244,24 @@ pub mod overture {
 
             // let mut previous_frame_time = Instant::now();
 
-            let mut imgui = imgui::Context::create();
-            let mut platform = imgui_winit_support::WinitPlatform::init(&mut imgui);
+            // let mut imgui = imgui::Context::create();
+            // let mut platform = imgui_winit_support::WinitPlatform::init(&mut imgui);
 
-            imgui.fonts().add_font(&[imgui::FontSource::TtfData {
-                data: include_bytes!("../assets/fonts/Antonio-Bold.ttf"),
-                size_pixels: 13.0,
-                config: Some(imgui::FontConfig {
-                    rasterizer_multiply: 1.5,
-                    oversample_h: 4,
-                    oversample_v: 4,
-                    ..imgui::FontConfig::default()
-                }),
-            }]);
+            // imgui.fonts().add_font(&[imgui::FontSource::TtfData {
+            //     data: include_bytes!("../assets/fonts/Antonio-Bold.ttf"),
+            //     size_pixels: 13.0,
+            //     config: Some(imgui::FontConfig {
+            //         rasterizer_multiply: 1.5,
+            //         oversample_h: 4,
+            //         oversample_v: 4,
+            //         ..imgui::FontConfig::default()
+            //     }),
+            // }]);
 
-            imgui.fonts().build_alpha8_texture();
-            imgui.fonts().build_rgba32_texture();
+            // imgui.fonts().build_alpha8_texture();
+            // imgui.fonts().build_rgba32_texture();
 
-            imgui.set_ini_filename(None);
+            // imgui.set_ini_filename(None);
 
             if let Some(event_loop) = self.event_loop.take() {
                 let _ = event_loop.run(move |event, event_loop| {
@@ -272,11 +272,11 @@ pub mod overture {
                             allowed_to_set_camera = false;
                         }
 
-                        platform.attach_window(
-                            imgui.io_mut(),
-                            &surface_state.window,
-                            imgui_winit_support::HiDpiMode::Rounded,
-                        );
+                        // platform.attach_window(
+                        //     imgui.io_mut(),
+                        //     &surface_state.window,
+                        //     imgui_winit_support::HiDpiMode::Rounded,
+                        // );
                     }
 
                     match event {
@@ -307,12 +307,12 @@ pub mod overture {
                                 // println!("{:?}ms", timestep * 1000.0);
 
                                 if let Some(ref surface_state) = self.surface_state {
-                                    if imgui.fonts().is_built() {
-                                        let ui = imgui.frame();
-                                        ui.show_demo_window(&mut true);
-                                        platform.prepare_render(ui, &surface_state.window);
-                                        imgui.render();
-                                    }
+                                    // if imgui.fonts().is_built() {
+                                    //     let ui = imgui.frame();
+                                    //     ui.show_demo_window(&mut true);
+                                    //     platform.prepare_render(ui, &surface_state.window);
+                                    //     imgui.render();
+                                    // }
 
                                     if let Some(ctx) = &self.context {
                                         if let Some(ref mut renderer) = self.render_state {
